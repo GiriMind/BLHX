7da785a523b697dfde6570ab5322c66c8c765ae8
GiriMind <GiriMind@users.noreply.github.com>
2018-04-28 21:44:18 +0800

    emmm

--- Window.py
+++ Window.py
@@ -10,6 +10,10 @@ import GraphCapCon as gcc
 
 class DesktopWindow:
     def __init__(self):
+        # self.hWnd = win32gui.FindWindow(None, "BlueStacks")
+        # if self.hWnd == 0:
+        #    raise Exception("{0} 窗口未找到。".format("BlueStacks"))
+
         print("正在枚举窗口……")
         windowList = gc.EnumWindows()
         for i in range(len(windowList)):
@@ -18,22 +22,18 @@ class DesktopWindow:
         i = int(input("请输入窗口编号："))
         self.window = windowList[i - 1]
 
-        # self.hWnd = win32gui.FindWindow(None, self.window.getName())
-        # if self.hWnd == 0:
-        #    raise Exception("{0} 窗口未找到。".format(self.window.getName()))
-
         self.capturer = gc.DesktopCapturer()
         self.image = gc.Image()
         self.rect = gc.Rect()
 
     def capture(self):
-        self.rect = self.window.getClientRect()
-
         # left, top, right, bottom = win32gui.GetWindowRect(self.hWnd)
         # self.rect.x = left
         # self.rect.y = top
         # self.rect.width = right - left
         # self.rect.height = bottom - top
+
+        self.rect = self.window.getRect()
 
         # print("窗口位置：x={0}, y={1}, width={2}, height={3}".format(
         #    self.rect.x, self.rect.y, self.rect.width, self.rect.height))
@@ -44,10 +44,19 @@ class DesktopWindow:
             raise Exception("图像格式错误：请将桌面设置为32位色。")
         # print("捕获位置：x={0}, y={1}, width={2}, height={3}".format(
         #    self.rect.x, self.rect.y, self.rect.width, self.rect.height))
-        return self.image
+        dsize = gc.Size(974, 634)
+        if self.rect.width != dsize.width and self.rect.height != dsize.height:
+            # print("调整捕获的图像大小至(974,634)。")
+            dimage = self.image.resize(dsize)
+            return dimage
+        else:
+            return self.image
 
     def click(self, location, size):
         x = self.rect.x + location.x + random.randint(0, size.width - 1)
         y = self.rect.y + location.y + random.randint(0, size.height - 1)
         pyautogui.moveTo(x, y)
         pyautogui.click()
+
+    def getPid(self):
+        return self.window.getPid()
