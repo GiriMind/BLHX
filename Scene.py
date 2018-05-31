f51b88fe8eb35ed519c29926e46845bc00408b49
GiriMind <GiriMind@users.noreply.github.com>
2018-05-31 13:52:48 +0800

    emmm

--- Scene.py
+++ Scene.py
@@ -47,39 +47,44 @@ class Scene:
 class PrecombatScene(Scene):
     def __init__(self, window):
         super().__init__(window)
-        self.c03s04Btn = Widget.Button(self.window, "3-4", "./Precombat/C03S04.png")
-        self.lkqwBtn = Widget.Button(self.window, "立刻前往", "./Precombat/LKQW.png")
+        self.c03s04Button = Widget.Button(self.window, "3-4", "./Precombat/C03S04.png")
+        self.goNowButton = Widget.Button(self.window, "立刻前往", "./Precombat/GoNow.png")
 
     def enterC03S04(self):
-        c03s04Target = self.matchButton(self.c03s04Btn, 5.0, 0.98)
+        c03s04Target = self.matchButton(self.c03s04Button, 5.0, 0.98)
         if c03s04Target is not None:
             c03s04Target.click()
             self.sleep()
 
-            lkqwTarget = self.matchButton(self.lkqwBtn, 5.0, 0.98)
-            if lkqwTarget is not None:
-                lkqwTarget.click()
+            goNowTarget = self.matchButton(self.goNowButton, 5.0, 0.98)
+            if goNowTarget is not None:
+                goNowTarget.click()
                 self.sleep()
 
-                lkqwTarget = self.matchButton(self.lkqwBtn, 5.0, 0.98)
-                if lkqwTarget is not None:
-                    lkqwTarget.click()
+                goNowTarget = self.matchButton(self.goNowButton, 5.0, 0.98)
+                if goNowTarget is not None:
+                    goNowTarget.click()
                     self.sleep()
 
 
 class C03S04Scene(Scene):
     def __init__(self, window):
         super().__init__(window)
-        self.zljdEnemy = Widget.Enemy(self.window, "主力舰队", "./SubChapter/ZLJD.png", "./SubChapter/ZLJDMask.png")
-        self.weighAnchorBtn = Widget.Button(self.window, "出击", "./SubChapter/WeighAnchor.png")
+        self.recFleetEnemy = Widget.Enemy(self.window, "侦查舰队", "./SubChapter/RecFleet.png",
+                                          "./SubChapter/RecFleetMask.png")
+        self.airFleetEnemy = Widget.Enemy(self.window, "航空舰队", "./SubChapter/AirFleet.png",
+                                          "./SubChapter/AirFleetMask.png")
+        self.mainFleetEnemy = Widget.Enemy(self.window, "主力舰队", "./SubChapter/MainFleet.png",
+                                           "./SubChapter/MainFleetMask.png")
+        self.weighAnchorButton = Widget.Button(self.window, "出击", "./SubChapter/WeighAnchor.png")
 
     def enterBattle(self):
-        zljdTargetList = self.matchEnemy(self.zljdEnemy, 5.0, 0.98)
+        zljdTargetList = self.matchEnemy(self.mainFleetEnemy, 5.0, 0.98)
         if len(zljdTargetList) > 0:
             zljdTargetList[0].click()
             self.sleep()
 
-            weighAnchorTarget = self.matchButton(self.weighAnchorBtn, 5.0, 0.98)
+            weighAnchorTarget = self.matchButton(self.weighAnchorButton, 5.0, 0.98)
             if weighAnchorTarget is not None:
                 weighAnchorTarget.click()
                 self.sleep()
@@ -88,28 +93,28 @@ class C03S04Scene(Scene):
 class BattleScene(Scene):
     def __init__(self, window):
         super().__init__(window)
-        self.ttcBtn = Widget.Button(self.window, "点击继续", "./Battle/TTC.png")
-        self.performanceBtn = Widget.Button(self.window, "性能", "./Battle/Performance.png")
-        self.confirmBtn = Widget.Button(self.window, "确认", "./Battle/Confirm.png")
+        self.ttcButton = Widget.Button(self.window, "点击继续", "./Battle/TTC.png")
+        self.performanceButton = Widget.Button(self.window, "性能", "./Battle/Performance.png")
+        self.confirmButton = Widget.Button(self.window, "确认", "./Battle/Confirm.png")
 
     def leaveBattle(self):
-        ttcTarget = self.matchButton(self.ttcBtn, 302.0, 0.98)
+        ttcTarget = self.matchButton(self.ttcButton, 310.0, 0.98)
         if ttcTarget is not None:
             ttcTarget.click()
             self.sleep()
 
-            ttcTarget = self.matchButton(self.ttcBtn, 5.0, 0.98)
+            ttcTarget = self.matchButton(self.ttcButton, 5.0, 0.98)
             if ttcTarget is not None:
                 ttcTarget.click()
                 self.sleep()
 
-                performanceTarget = self.matchButton(self.performanceBtn, 5.0, 0.98)
+                performanceTarget = self.matchButton(self.performanceButton, 5.0, 0.98)
                 if performanceTarget is not None:
                     target = Widget.Target(self.window, gc.Point(100, 100), gc.Size(800, 320))
                     target.click()
                     self.sleep()
 
-                confirmTarget = self.matchButton(self.confirmBtn, 5.0, 0.98)
+                confirmTarget = self.matchButton(self.confirmButton, 5.0, 0.98)
                 if confirmTarget is not None:
                     confirmTarget.click()
                     self.sleep()
